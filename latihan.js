cb6d42d47d6f9799ad0bd023857ab3441ed299af
AIS912 <faisalabdulhalim912@gmail.com>
2024-08-18 22:22:23 +0700

    Add files via upload

---
+++ latihan.js
@@ -0,0 +1,62 @@
+// soal 1
+// function formatDate () {
+//     var day = "21";
+//     var month = "july";
+//     var year = "2024";
+//     return day + '/' + month + '/' + year;
+//    }
+//    var date = formatDate()
+//    console.log(date)
+
+//soal 2
+// function filterEvenNumbers(numbers) {
+//     return numbers.filter(function(number) {
+//       return number % 2 == 0;
+//     });
+//   }
+  
+//   let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//   let evenNumbers = filterEvenNumbers(numbers);
+//   console.log(evenNumbers);
+  
+//soal 3
+  // function wordCount(sentence) {
+  //     let words = sentence.trim().split(/\s+/);
+  //     return words.length;
+  //   }
+  //   let sentence = "Hallo Nama Saya Faisal.";
+  //   let count = wordCount(sentence);
+  //   console.log(count);
+
+  //soal 4
+  // function reverseString(str) {
+  //   let charArray = str.split('');
+    
+  //   charArray.reverse();
+    
+  //   let reversedString = charArray.join('');
+    
+  //   return reversedString;
+  // }
+  
+  // let originalString = "Faisal Abdul Halim";
+  // let reversed = reverseString(originalString);
+  // console.log(reversed); 
+
+  //soal 5
+  // function findMax(numbers) {
+  //   if (numbers.length === 0) {
+  //     throw new Error("Array tidak boleh kosong");
+  //   }
+  
+  //   let max = numbers.reduce(function (acc, current) {
+  //     return current > acc ? current : acc;
+  //   }, numbers[0]);
+  
+  //   return max;
+  // }
+  
+  // let numbers = [3, 5, 7, 2, 8, -1, 4, 10, 12];
+  // let maxNumber = findMax(numbers);
+  // console.log(maxNumber); 
+  
